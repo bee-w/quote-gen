c15b51fab29f21dd2769996996b261b280645b81
Brigette Maher <bv1232@outlook.com.au>
2021-11-24 16:40:38 +1100

    corrected

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('testing');
+let apiQuotes = [];
+
+// Get quotes from API: type.fit/api/quotes
+async function getQuotes() {
+    const apiUrl = 'type.fit/api/quotes';
+    try {
+        // await so that fetched only after data loaded
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes);
+    } catch (error) {
+        // catch error
+    }
+}
+
+// on load of webpage
+getQuotes();
